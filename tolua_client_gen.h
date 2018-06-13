361d9422d86b3be6026dc3d060439900a1aa7bdf
The Reverend Cozmo <revcozmo33@aol.com>
2018-06-12 21:25:58 -0400

    Add files via upload

---
+++ tolua_client_gen.h
@@ -0,0 +1,9 @@
+/*
+** Lua binding: client
+** Generated automatically by tolua 5.2.0 on Sat Mar 24 12:21:56 2018.
+*/
+
+/* Exported function */
+TOLUA_API int tolua_client_open (lua_State* tolua_S);
+LUALIB_API int luaopen_client (lua_State* tolua_S);
+
