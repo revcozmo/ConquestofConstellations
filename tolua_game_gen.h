2c1e772aa1cd91e2a62ac60807418d234d7930ee
The Reverend Cozmo <revcozmo33@aol.com>
2018-06-12 21:31:23 -0400

    Add files via upload

---
+++ tolua_game_gen.h
@@ -0,0 +1,9 @@
+/*
+** Lua binding: game
+** Generated automatically by tolua 5.2.0 on Sat Mar 24 12:19:23 2018.
+*/
+
+/* Exported function */
+TOLUA_API int tolua_game_open (lua_State* tolua_S);
+LUALIB_API int luaopen_game (lua_State* tolua_S);
+
